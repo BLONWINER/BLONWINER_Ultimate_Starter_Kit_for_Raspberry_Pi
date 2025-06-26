d2c5547b0b501bdcec86d70eae5cf5b8d39d0f12
blonwiner@outlook.com <39189248+BLONWINER@users.noreply.github.com>
2025-06-26 12:21:33 +0800

    Add files via upload

---
+++ Code/Python_GPIOZero_Code/00.0.0_Hello/Hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+########################################################################
+# Filename    : Hello.py
+# Description : Print "Hello World!".
+# auther      : BLONWINER
+# modification: 2025/01/01
+########################################################################
+
+def Hello():
+    print('Hello World!')
+
+Hello()
